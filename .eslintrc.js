c666ae7f2873f19177f9d5ea951429dc72fa3f28
GuyARoss <guyallenross@gmail.com>
2020-01-02 18:33:30 -0500

    chore (updated eslint)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,31 @@
 module.exports = {
     "env": {
-        "es6": true,
-        "node": true
+        "browser": true,
+        "es6": true
     },
-    "extends": "google",
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
     "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
         "ecmaVersion": 2018,
         "sourceType": "module"
     },
+    "plugins": [
+        "react"
+    ],
     "rules": {
-        "max-len": "error",
+        "semi": ["error", "always"],
+        "quotes": ["error", "single"],
+        "eol-last": ["error", "always"],
+        "max-len": ["error", { "code": 80 }],
+        "max-depth": [1, 3]
     }
 };
