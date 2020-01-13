77e448f059dda8c5665dd1537143e4446444210d
Hashed <z-donnelly@hvcc.edu>
2020-01-12 19:43:55 -0500

    branch change for pull

--- src/containers/memory-visualizer/index.js
+++ src/containers/memory-visualizer/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 import { data } from './mock-data';
+import { request } from 'graphql-request';
 
 import Layout from 'containers/layout';
 import './memory.scss';
@@ -12,5 +13,14 @@ const MemoryVisualizer = () => {
     </div>
   );
 };
+
+const query = `
+  query UserProfile {
+
+  }
+`;
+request('http://localhost:3006/graphql', query)
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
 
 export default Layout(MemoryVisualizer);
