f176529cab03f6688a7a12f5b35136a9d341d70c
Guy Ross <guyallenross@gmail.com>
2020-02-06 19:47:11 -0500

    rm

--- src/containers/analytics-dashboard/index.js
+++ src/containers/analytics-dashboard/index.js
@@ -4,23 +4,17 @@ import { Typography } from 'antd';
 
 import Layout from 'containers/layout';
 import ServicesHealthStatus from 'components/services-health-status';
-import TimespanLinegraph from 'components/timespan-linegraph';
 import enhance from './enhance-analytics-dashboard';
 
 const AnalyticsDashboard = enhance(({ services }) => (
     <>
-        <Typography.Title level={3}> Active Users </Typography.Title>
-        <TimespanLinegraph  
-            color="20, 120, 240"
-            data={[1, 2, 3]}
-            fill
-        />
-
         <Typography.Title level={3}> Service Health </Typography.Title>
         
-        <ServicesHealthStatus
-            services={services}
-        />
+        <div style={{ width: '40%', minWidth: 200 }}>
+            <ServicesHealthStatus
+                services={services}
+            />
+        </div>
     </>
 ));
 
